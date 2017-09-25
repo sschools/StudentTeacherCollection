4acdcafbcec4db2f0274bd9d0b4692708174b48a
Stephen Schools <stephen.schools>
2017-09-25 14:09:09 -0400

    initial commit

---
+++ src/com/ironyard/Student.java
@@ -0,0 +1,43 @@
+package com.ironyard;
+
+public class Student {
+    String firstName;
+    String lastName;
+    int id;
+    int grade;
+
+    public Student() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+}
