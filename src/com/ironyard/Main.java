673ab4ce97e18939202648495e0df7f70de20058
Stephen Schools <stephen.schools>
2017-09-25 14:43:49 -0400

    students list set up

--- src/com/ironyard/Main.java
+++ src/com/ironyard/Main.java
@@ -1,8 +1,21 @@
 package com.ironyard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+	public static void main(String[] args) {
+		List<Student> students = new ArrayList<>();
+		String[] firstNames = {"Sam", "Caroline", "Joe", "Sue", "Bob", "Mike", "Carol", "Greg", "Peter", "Bobby", "Marcia", "Jan", "Cindy", "Alice", "Oliver"};
+		String[] lastNames = {"Smith", "Adams", "Jones", "Brady", "David", "Seinfeld", "Kramer", "Benes", "Costanza", "Sacomano", "Newman", "Childs", "Green", "Hines", "Lewis"};
+		for (int i = 0; i < 15; i++) {
+			Student temp = new Student();
+			temp.setFirstName(firstNames[i]);
+			temp.setLastName(lastNames[i]);
+			temp.setId(i);
+			temp.setGrade(1);
+			students.add(temp);
+		}
+	}
 }
